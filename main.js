4770d6eea6c9e0186f85a749579a0b3d89b5a758
Geoffrey Winegar <wgwin@LAPTOP-D8TE94H0.mit.edu>
2018-09-13 15:22:55 -0400

    monalisa

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 
 //----------------------------------
-//          YOUR CODE
+//createImageBitmap(100,100,'black');
+// substitute black with value you read from ___
+// dimensions are 81x53
+var counter = 0;
+for (var j = 0; j < 81, j++){
+    y = j*15;
+    for (var i = 0; i < 53; i++){
+        x = i*15;
+        create(x,y,colors[counter]);
+        counter++;
+    }
+}
 //----------------------------------
